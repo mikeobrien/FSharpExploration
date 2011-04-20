8ad755c41a0d0c45fbf6349f732bfb08a9ca798c
Mike O'Brien <mob@mikeobrien.net>
2011-04-20 15:28:15 -0400

    Added FsUnit ref.

--- src/Option.fs
+++ src/Option.fs
@@ -1,3 +1,10 @@
-﻿module Option
+﻿namespace FSharpExploration.Tests
 
-    
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>] 
+type ``The option type`` () =
+
+    [<Test>] member test.``shold do this.`` () =
+        5 = 5 |> should be True
